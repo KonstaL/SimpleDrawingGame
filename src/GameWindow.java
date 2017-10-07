a27c64ecc544a896af9d82302574df1efc18f809
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2017-10-07 16:03:05 +0300

    Move AskUsername from class to a method, as it doesn't work inside a class

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,8 +1,16 @@
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class GameWindow extends JFrame {
     public GameWindow(int width, int height) {
         setSize(width, height);
         setVisible(true);
+        askUsername();
+        
+    }
+
+    public String askUsername() {
+        String player1;
+        return JOptionPane.showInputDialog(null, "Please enter name");
     }
 }
