a070dfb0200457c4cb4a82747059d05fb62b348c
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2017-10-07 22:23:36 +0300

    add constuctor to Point

--- src/Point.java
+++ src/Point.java
@@ -5,6 +5,11 @@ import java.awt.Graphics;
 public class Point extends JPanel {
     private int x;
     private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
     
     @Override
     public void paintComponent(Graphics g) {
