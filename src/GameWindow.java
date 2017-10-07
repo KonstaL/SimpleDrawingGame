087a2e9c437441e21ce598ec6de04de175d034f5
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2017-10-07 16:15:45 +0300

    Added functionality to askUsername

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -10,8 +10,12 @@ public class GameWindow extends JFrame {
         
     }
 
-    public String askUsername(Component parent) {
-        String player1;
-        return JOptionPane.showInputDialog(parent, "Please enter name");
+    public String[] askUsername(Component parent) {
+        String[] players = new String[2]; //Hardcoded to string and limited to 2 for now
+        do {
+            players[0] = JOptionPane.showInputDialog(parent, "Player 1!\nPlease enter name");
+            players[1] = JOptionPane.showInputDialog(parent, "Player 2!\nPlease enter name");
+        } while (players[0].length() <= 0 || players[1].length() <= 0);
+        return players;
     }
 }
