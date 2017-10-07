102a4e4404d5e0eacd0f573a14c8d0fc64987175
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2017-10-07 16:05:43 +0300

    Add parent component to AskUsername

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,16 +1,17 @@
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import java.awt.Component;
 
 public class GameWindow extends JFrame {
     public GameWindow(int width, int height) {
         setSize(width, height);
         setVisible(true);
-        askUsername();
+        askUsername(this);
         
     }
 
-    public String askUsername() {
+    public String askUsername(Component parent) {
         String player1;
-        return JOptionPane.showInputDialog(null, "Please enter name");
+        return JOptionPane.showInputDialog(parent, "Please enter name");
     }
 }
