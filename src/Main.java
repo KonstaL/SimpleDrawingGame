90d32baa3b7fd2a9f6ef6b161edb1c71fa3063f3
Niksutin <niko.mattila@cs.tamk.fi>
2017-10-08 19:00:33 +0300

    Move all code from main to new method show(). Initialize few variables

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,31 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 
 public class Main {
+
+    JButton clearBtn, blackBtn;
+    DrawArea drawArea;
+    ActionListener actionlistener = new ActionListener() {
+
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == clearBtn) {
+
+            } else if (e.getSource() == blackBtn) {
+                
+            }
+        }
+    };
+
     public static void main(String[] args) {
+    }
+
+    public void show() {
         GameWindow test = new GameWindow(400, 400);
         Container content = test.getContentPane();
         content.setLayout(new BorderLayout());
         final DrawArea drawArea = new DrawArea();
         content.add(drawArea, BorderLayout.CENTER);
+        JPanel controls = new JPanel();
+        clearBtn = new JButton("Clear");
+        blackBtn = new JButton("Black");
     }
 }
