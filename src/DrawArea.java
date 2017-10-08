5363c93de0dbce933bdc661f9ed1b4c79132734f
Niksutin <niko.mattila@cs.tamk.fi>
2017-10-08 17:57:15 +0300

    Add comments on DrawArea variables

--- src/DrawArea.java
+++ src/DrawArea.java
@@ -1,4 +1,11 @@
+
 class DrawArea extends JComponent {
+
+    // Image in which we're going to draw
     private Image image;
+
+    // Gtraphics2D object ==> used to draw on
     private Graphics2D graphics;
+    
+
 }
