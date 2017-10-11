aa7b11d9bf9a32ee7fafb7297190f85d389a173f
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2017-10-11 18:27:21 +0300

    GameWindow: Remove unnecessary imports

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,16 +1,4 @@
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseEvent;
-import java.util.List;
-import java.util.ArrayList;
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.util.LinkedList;
-
 
 public class GameWindow extends JFrame {
 
