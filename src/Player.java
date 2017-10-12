c49656e9870daec1dbd8b473be9aeac21e72bd88
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2017-10-12 18:56:23 +0300

    Player: Add player constructor, attributes and get/set methods

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,33 @@
 public class Player {
+    private String name;
+    private String guess;
+    private int points = 0;
 
+    public Player(String s) {
+        this.name = s;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points += points;
+    }
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
 }
