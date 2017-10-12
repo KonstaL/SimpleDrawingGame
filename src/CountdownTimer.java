391735455b086116caf8f542f37ebee7e70896c4
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2017-10-13 02:12:18 +0300

    CountdownTimer: Implement timer functionality

--- src/CountdownTimer.java
+++ src/CountdownTimer.java
@@ -1,7 +1,22 @@
 import javax.swing.*;
 public class CountdownTimer extends JLabel {
+    private int remainingTime = 60;
+    private Timer timer;
     public CountdownTimer() {
-        Timer timer = new Timer(1000, (e) -> repaint());
-        timer.start();
+        timer = new Timer(1000, (e) -> {
+            remainingTime--;
+            repaint();
+        });
+    }
+    public Timer getTimer() {
+        return this.timer;
+    }
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    @Override
+    public String getText() {
+        return Integer.toString(remainingTime);
     }
 }
