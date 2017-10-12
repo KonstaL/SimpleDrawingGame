b9d0fd071f1868331aa579fb5e7072553059ddc3
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2017-10-13 01:47:58 +0300

    CountdownTimer: Add constructor method to class CountdownTimer

--- src/CountdownTimer.java
+++ src/CountdownTimer.java
@@ -1,3 +1,7 @@
-public class CountdownTimer {
-
+import javax.swing.*;
+public class CountdownTimer extends JLabel {
+    public CountdownTimer() {
+        Timer timer = new Timer(1000, (e) -> repaint());
+        timer.start();
+    }
 }
